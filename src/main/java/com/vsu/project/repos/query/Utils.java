7e5cf71a75598e901e036475d84cc991965c2f4e
Артем <artem-solovyev@mail.ru>
2021-04-02 11:22:54 +0300

    first

--- src/main/java/com/vsu/project/repos/query/Utils.java
+++ src/main/java/com/vsu/project/repos/query/Utils.java
@@ -1,5 +1,5 @@
 package com.vsu.project.repos.query;
 
 public class Utils {
-    private final static String BID_AVAILABLE="select no ia ego lublu";
+    private final static String BID_AVAILABLE="";
 }

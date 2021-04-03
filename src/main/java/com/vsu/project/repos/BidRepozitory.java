32cfd73eafb99a3e2e9ac41b3bcead74101ba75e
Артем <artem-solovyev@mail.ru>
2021-04-03 08:53:06 +0300

    context miss commit

--- src/main/java/com/vsu/project/repos/BidRepozitory.java
+++ src/main/java/com/vsu/project/repos/BidRepozitory.java
@@ -1,9 +1,9 @@
-//package com.vsu.project.repos;
-//
-//import com.vsu.project.models.Bid;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface BidRepozitory extends JpaRepository <Bid,Long> {
-//}
+package com.vsu.project.repos;
+
+import com.vsu.project.models.Bid;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BidRepozitory extends JpaRepository <Bid,Long> {
+}
